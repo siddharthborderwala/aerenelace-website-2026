86e0ce61b127b0152f78e220ee651064b789410c
Siddharth Borderwala <siddharthborderwala@gmail.com>
2026-01-01 19:24:43 +0530

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Public_Sans, Playfair_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import "./globals.css";
@@ -40,6 +41,7 @@ export default function RootLayout({
         <Header />
         <div className="flex-1">{children}</div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
